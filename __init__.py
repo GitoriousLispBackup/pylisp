af5342eb69e39b3b8bfd7776f926b57458e560d7
yoch <yoch.melka@gmail.com>
2013-07-05 13:17:50 +0300

    remove some internals refs from package

--- __init__.py
+++ __init__.py
@@ -6,4 +6,4 @@ sys.path.append(os.path.dirname(__file__))
 from lisp_lexer import lisp_lexer
 from lisp_parser import lisp_parser
 from lisp_errors import LispError, LispParseError
-from lisp import _Fvals as Fvals, nil
+
